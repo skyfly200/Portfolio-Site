cb219bb7bd6c374d711ba89c5dc9fcebbe7f2b38
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-07 11:01:43 -0700

    Test Blog View Working, Organized file from existing project into respective locations for refactoring with vue

--- src/router.js
+++ src/router.js
@@ -23,6 +23,15 @@ export default new Router({
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
+      path: "/blog",
+      name: "blog",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Blog.vue")
+    },
+    {
       path: "*",
       name: "404",
       component: () => import(/* webpackChunkName: "404" */ "./views/404.vue")
