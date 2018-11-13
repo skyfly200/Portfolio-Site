bd4d0ba398a978e44bd7c2f2eeb774566f4f8362
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-13 00:15:09 -0700

    vue-flux slider working with kitten sample images

--- src/components/skills/Photo.vue
+++ src/components/skills/Photo.vue
@@ -1,44 +1,42 @@
 <template lang="pug">
   .gallery.skill
-    .slides
-      .slide
-        img(src="/images/bkgs/20140615_161556.jpg")
-      .slide
-        img(src="/images/bkgs/20140616_084526.jpg")
-      .slide
-        img(src="/images/bkgs/20140705_132704-d1.jpg")
-      .slide
-        img(src="/images/bkgs/20140729_175439.jpg")
-      button(onclick="plusDivs(-1)") &#10094;
-      button(onclick="plusDivs(+1)") &#10095;
+    vue-flux(
+      :options="fluxOptions"
+      :images="fluxImages"
+      :transitions="fluxTransitions"
+      ref="slider")
+    button(@click="$refs.slider.showImage('next')") &#10094;
+    button(@click="$refs.slider.showImage('previous')") &#10095;
 </template>
 
 <script>
+import { VueFlux, Transitions } from "vue-flux";
+
 export default {
   name: "photo",
-  components: {}
+  components: {
+    VueFlux
+  },
+  data: () => ({
+    fluxOptions: {
+      autoplay: true
+    },
+    fluxImages: [
+      "http://placekitten.com/1000/800",
+      "http://placekitten.com/1000/700",
+      "http://placekitten.com/1000/701",
+      "http://placekitten.com/1000/702",
+      "http://placekitten.com/1000/900"
+    ],
+    fluxTransitions: {
+      transitionBook: Transitions.transitionCamera
+    }
+  })
 };
 </script>
 
 <style lang="sass">
   .gallery
     text-align: center
-    &.topic-expanded
-      display: flex
-      align-items: center
-    .gallery
-      .slides
-        button
-          color: #000
-      .slide
-        display: none
-        img
-          max-width: 100%
-          max-height: 95vh
-    @media (max-width: 800px)
-      padding: 0px
-      .video-player
-        iframe
-          width: 480px
-          height: 360px
+    height: 80vh
 </style>
