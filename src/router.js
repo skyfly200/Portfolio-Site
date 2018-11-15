dadfcecfd3bc329ef7067746ae6c26055702112b
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-15 12:35:32 -0700

    change routers blog chunk name & set redirect for SPA in "netliify.toml"

--- src/router.js
+++ src/router.js
@@ -19,8 +19,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Blog.vue")
+      component: () => import(/* webpackChunkName: "blog" */ "./views/Blog.vue")
     },
     {
       path: "*",
