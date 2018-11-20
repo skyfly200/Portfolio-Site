70d810edec98128c69e9e19cdba177352c3bced2
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-20 10:42:55 -0700

    fix some styles

--- src/components/blog/Post.vue
+++ src/components/blog/Post.vue
@@ -34,7 +34,7 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
   .post
     width: 90%
     padding: 1em
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -51,7 +51,7 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
   body
     margin: 0
     .wrapper
