2cef5da79f9cab2f04f33df16444216f70b6535b
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-27 18:13:25 -0700

    add test fot DevOps

---
+++ tests/unit/components/skills/DevOps.spec.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import DevOps from "@/components/skills/DevOps.vue";
+
+describe("DevOps.vue", () => {
+  it("Renders Title", () => {
+    const wrapper = shallowMount(DevOps);
+    expect(wrapper.text()).to.include("Experience and Skillset");
+  });
+});
