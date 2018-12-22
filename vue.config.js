968fe088f812a933c8768136290501730e67541e
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-12-22 13:47:30 -0700

    prettier fix

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
         .use("sass-resources-loader")
         .loader("sass-resources-loader")
         .options({
-          resources: ["./src/styles/vars.sass",   "./src/styles/mixins.sass"]
+          resources: ["./src/styles/vars.sass", "./src/styles/mixins.sass"]
         })
         .end();
     });
