f1da6ce1572cdde82899f2ab42878df028ecf08d
Skyler Fly-Wilson <magic@skylerfly.com>
2024-12-14 21:13:22 -0700

    merge old vuetify imports into new version

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,39 +1,10 @@
 // vuetify.js
+/*eslint no-unused-vars: ["error", { "args": "none" }]*/
 import { createVuetify } from "vuetify";
-import { aliases, fa } from "vuetify/iconsets/fa";
-
-const vuetify = createVuetify({
-  theme: {
-    defaultTheme: "light",
-    themes: {
-      light: {
-        colors: {
-          primary: "#7627D0",
-          secondary: "#6608D2",
-          accent: "#4D059E",
-        }
-      }
-    }
-  },
-  icons: {
-    defaultSet: "fa",
-    aliases,
-    sets: {
-      fa
-    }
-  }
-});
-
-export default vuetify;
-
-
-// /*eslint no-unused-vars: ["error", { "args": "none" }]*/
-// import Vue from "vue";
-// import Vuetify, {
+// import {
 //   VApp, // required
 //   VBtn,
 //   VCard,
-//   VCardMedia,
 //   VCardTitle,
 //   VCardText,
 //   VCardActions,
@@ -60,54 +31,77 @@ export default vuetify;
 //   VToolbar,
 //   VToolbarSideIcon,
 //   VToolbarTitle
-// } from "vuetify/lib";
-// import "vuetify/src/stylus/app.styl";
-// import { Ripple } from "vuetify/lib/directives";
+// } from "vuetify/components";
+//import "vuetify/src/stylus/app.styl";
+import { Ripple } from "vuetify/directives";
+import { aliases, fa } from "vuetify/iconsets/fa";
 
-// Vue.use(Vuetify, {
-//   components: {
-//     VApp,
-//     VBtn,
-//     VCard,
-//     VCardMedia,
-//     VCardTitle,
-//     VCardText,
-//     VCardActions,
-//     VChip,
-//     VCombobox,
-//     VContainer,
-//     VContent,
-//     VDivider,
-//     VFlex,
-//     VFooter,
-//     VForm,
-//     VIcon,
-//     VLayout,
-//     VList,
-//     VListTile,
-//     VListTileContent,
-//     VListTileTitle,
-//     VMenu,
-//     VNavigationDrawer,
-//     VSelect,
-//     VSwitch,
-//     VTextarea,
-//     VTextField,
-//     VToolbar,
-//     VToolbarSideIcon,
-//     VToolbarTitle
-//   },
-//   directives: {
-//     Ripple
-//   },
-//   iconfont: "fa",
-//   theme: {
-//     primary: {
-//       base: "#7627D0",
-//       darken1: "#6608D2",
-//       darken2: "#4D059E",
-//       darken3: "#3E0480",
-//       darken4: "#2E0260"
+// components: {
+//   VApp,
+//   VBtn,
+//   VCard,
+//   VCardTitle,
+//   VCardText,
+//   VCardActions,
+//   VChip,
+//   VCombobox,
+//   VContainer,
+//   VContent,
+//   VDivider,
+//   VFlex,
+//   VFooter,
+//   VForm,
+//   VIcon,
+//   VLayout,
+//   VList,
+//   VListTile,
+//   VListTileContent,
+//   VListTileTitle,
+//   VMenu,
+//   VNavigationDrawer,
+//   VSelect,
+//   VSwitch,
+//   VTextarea,
+//   VTextField,
+//   VToolbar,
+//   VToolbarSideIcon,
+//   VToolbarTitle
+// },
+
+const vuetify = createVuetify({
+  directives: {
+    Ripple
+  },
+  theme: {
+    primary: {
+      base: "#7627D0",
+      darken1: "#6608D2",
+      darken2: "#4D059E",
+      darken3: "#3E0480",
+      darken4: "#2E0260"
+    }
+  },
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa
+    }
+  }
+});
+
+export default vuetify;
+
+// iconfont: "fa",
+// theme: {
+//   defaultTheme: "light",
+//   themes: {
+//     light: {
+//       colors: {
+//         primary: "#7627D0",
+//         secondary: "#6608D2",
+//         accent: "#4D059E",
+//       }
 //     }
 //   }
-// });
+// },
