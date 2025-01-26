1a803a0d5b1a8d16b56cd209c79b96e5bc8930e7
Skyler Fly-Wilson <magic@skylerfly.com>
2025-01-26 03:05:24 -0700

    update and redesign the linux page

--- src/views/Linux.vue
+++ src/views/Linux.vue
@@ -1,32 +1,60 @@
 <template lang="pug">
 .back-end.skill
-  v-fab(to="/software" flat location="top start" app).home-btn Back
-  .skills-list
-    h2 Linux Skills (Dev Ops)
-    ul
-      li Apache, Nginx, Express
-      li MySql, MariaDB, SQLite, Mongo
-      li Python, C, PHP, Node
-      li SSH, SCP, SFTP
-      li Bash, Shell, Cron
-      li Networks
-      li Docker
-      li Virtual Box
-      li Digital Ocean Spaces
-      li Lets Encrypt Certbot
-      li Icecast2
-
-  .projects-list
-    h2 Some of My Projects
-    ul
-      li
-        a(href="//github.com/skyfly200/CCRN-Website" target="_blank") CCRN Website
-      li
-        a(href="//wayhighradio.com/listen.html" target="_blank") Way High Radio Streaming Servers
-      li
-        a(href="//github.com/skyfly200/Radio-Network-Hub" target="_blank") Radio Network Control System
-
-      GitHubBtn
+  v-fab(to="/software" flat location="top start" app).home-btn Back  
+  v-container
+    v-row.skills-list
+      v-col(cols="12")
+          h2 Dev Ops Skills
+          v-chip-group
+            v-chip Apache
+            v-chip Nginx
+            v-chip Express
+            v-chip Node
+            v-chip MySql
+            v-chip MariaDB
+            v-chip SQLite
+            v-chip Mongo
+            v-chip Python
+            v-chip Ruby
+            v-chip PHP
+            v-chip SSH
+            v-chip CodeCov
+            v-chip Pupeteer
+            v-chip Bash/Shell
+            v-chip Cron
+            v-chip TensorFlow
+            v-chip Ansible
+            v-chip Docker
+            v-chip Virtual Box
+            v-chip Digital Ocean
+            v-chip AWS
+            v-chip Google Cloud Platform
+            v-chip Firebase
+            v-chip Netlify
+            v-chip GitHub
+            v-chip Lets Encrypt Certbot
+            v-chip Icecast2
+    v-row.projects-list
+      v-col(cols="12")
+        h2 Some of Dev Ops / Linux Projects
+      v-col(cols="12" md="6")
+        v-card
+          v-card-title CCRN Website
+          v-card-subtitle Website for CCRN
+          v-card-actions.card-actions
+            v-btn.card-btn(variant="outlined" color="accent" block href="//github.com/skyfly200/CCRN-Website" target="_blank") Visit Site
+      v-col(cols="12" md="6")
+        v-card
+          v-card-title Way High Radio Streaming Servers
+          v-card-subtitle Streaming Servers for Way High Radio
+          v-card-actions.card-actions
+            v-btn.card-btn(variant="outlined" color="accent" block href="//wayhighradio.com/listen.html" target="_blank") Visit Site
+      v-col(cols="12" md="6")
+        v-card
+          v-card-title Radio Network Control System
+          v-card-subtitle Control System for Radio Network
+          v-card-actions.card-actions
+            v-btn.card-btn(variant="outlined" color="accent" block href="//github.com/skyfly200/Radio-Network-Hub" target="_blank") Visit Site
 
 </template>
 
@@ -50,6 +78,9 @@ export default {
     margin: 0px auto
   @media (max-width: 660px)
     flex-direction: column
+  
+  .v-chip
+    margin: 5px
   
   .home-btn
     a
