7b62ce6058e50aaaff596db01126ddb31a6cc0d0
Skyler Fly-Wilson <magic@skylerfly.com>
2025-02-05 14:23:07 -0700

    add img optimizer plugin for vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path'; // Import path for resolving aliases
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Define the '@' alias for src
