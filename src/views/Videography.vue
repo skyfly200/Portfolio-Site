508caac03e3d67d5b70ed10a2878a120ece79571
Skyler Fly-Wilson <magic@skylerfly.com>
2025-02-18 12:15:50 -0700

    add way high promom video to the video page

--- src/views/Videography.vue
+++ src/views/Videography.vue
@@ -9,10 +9,31 @@
     .v-row(sm="12")
       .v-card(style="width: 100%")
         .v-card-title
+          h3.primary-title Matajur Peak Timelapse
+          h4.subtitle-1 A 90 minute timelapse looking out over Slovenia's Soča River Valley from on top of Matajur Peak in 2024
+        .v-card-text.video-player
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/KmDGvHkVhvk?si=AjnIDopAG_EvvAmv" title="Cloud Sea Timelapse" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+    .v-row(sm="12")
+      .v-card(style="width: 100%")
+        .v-card-title
           h3.primary-title American Ninja Warrior Entry Video
           h4.subtitle-1 Produced for my friend Critter in 2015
         .v-card-text.video-player
-          iframe(src="https://www.youtube.com/embed/9o6mhFOJO5c?rel=0" frameborder="0" allowfullscreen)
+          iframe(src="https://www.youtube.com/embed/9o6mhFOJO5c?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+    .v-row(sm="12")
+      .v-card(style="width: 100%")
+        .v-card-title
+          h3.primary-title Cloud Sea Timelapse
+          h4.subtitle-1 Filmed near Nederland, CO
+        .v-card-text.video-player
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/L46CGIbLuAg?si=ypXafbe4WhIdw69S" title="Cloud Sea Timelapse" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
+    .v-row(sm="12")
+      .v-card(style="width: 100%")
+        .v-card-title
+          h3.primary-title Way High Radio Promo Video
+          h4.subtitle-1 Produced in 2015
+        .v-card-text.video-player
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/gYpYreUpXYk?si=sJokumzSesxNq6mq" title="Cloud Sea Timelapse" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen)
 </template>
 
 <script>
