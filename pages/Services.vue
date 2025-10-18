bd6716d1f44c4ad37a155d1e813e362101564d11
Skyler Fly-Wilson <magic@skylerfly.com>
2025-10-18 02:57:58 -0600

    improve design and phrasing of the services page

--- pages/Services.vue
+++ pages/Services.vue
@@ -2,7 +2,7 @@
 .services.skill
   //- v-fab(to="/" flat location="top start" app).home-btn Home
   .services-info
-    h2 IT Solutions, Tutoring and Consulting
+    h2.header IT, Tutoring and Consulting Services
     v-container
       v-row
         v-col(cols="12" md="6")
@@ -10,7 +10,7 @@
             v-img(cover src="https://picsum.photos/id/1078/600/300" alt="Buisnees IT Image").card-img
             v-card-title Buisness IT Solutions
             v-card-subtitle All your IT needs in one place
-            v-card-text
+            v-card-text(height="10em")
               | I provide comprehensive IT solutions for small to medium-sized businesses, including network setup and management, cybersecurity, cloud services, and hardware/software procurement. My goal is to help businesses streamline their operations and enhance productivity through effective technology solutions.
             v-card-actions.card-actions
               v-btn.card-btn(variant="outlined" color="accent" block to="/buis-it") See More
@@ -19,8 +19,8 @@
             v-img(cover src="https://picsum.photos/id/225/600/300" alt="Home IT Image").card-img
             v-card-title  Home IT Support
             v-card-subtitle In Home and Remote IT Support for all your devices
-            v-card-text 
-              | I offer in-home and remote IT support services for individuals and families, helping with computer setup, troubleshooting, software installation, virus removal, and general tech support. My goal is to make technology accessible and easy to use for everyone.
+            v-card-text(height="10em")
+              | I offer in-home and remote IT support services, helping with computer setup, troubleshooting, and general tech support.
             v-card-actions.card-actions
               v-btn.card-btn(variant="outlined" color="accent" block to="/home-it") See More
       v-row
@@ -29,8 +29,8 @@
             v-img(cover src="https://picsum.photos/id/1073/600/300" alt="Tutoring Image").card-img
             v-card-title  Tutoring
             v-card-subtitle Personalized IT and Software Development Tutoring
-            v-card-text 
-              | I offer personalized tutoring services in various IT and software development topics, including programming languages (Python, JavaScript, etc.), web development, database management, and cloud computing. My tutoring sessions are tailored to meet the individual needs of each student, whether they are beginners or looking to advance their skills.
+            v-card-text(height="10em")
+              | I offer personalized tutoring services in various IT and software development topics, including programming languages (Python, JavaScript, etc.), web development, database management, and cloud computing. My tutoring sessions are tailored to meet the individual needs of each student.
             v-card-actions.card-actions
               v-btn.card-btn(variant="outlined" color="accent" block to="/tutoring") See More
         v-col(cols="12" md="6")
@@ -38,7 +38,7 @@
             v-img(cover src="https://picsum.photos/id/306/600/300" alt="Consulting Image").card-img
             v-card-title Emerging Technologies Consulting
             v-card-subtitle Expert IT Consulting for Businesses and Individuals
-            v-card-text
+            v-card-text(height="10em")
               | I provide expert consulting services on emerging technologies such as blockchain, artificial intelligence, automation, EVs, renewable energy and cloud computing. I help businesses and individuals understand the potential applications of these technologies and how they can be leveraged to achieve their goals.
             v-card-actions.card-actions
               v-btn.card-btn(variant="outlined" color="accent" block to="/consulting") See More
@@ -69,6 +69,10 @@ export default {
     text-align: center
     margin: auto
     padding: 10px
+
+  .header
+    margin-bottom: 20px
+    margin-top: 20px
 
   .card-img
     height: 300px
